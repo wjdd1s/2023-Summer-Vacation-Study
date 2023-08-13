e57e7b85dda4901d67e8c573e19484d2476e1bdb
wjdd1s <141038368+wjdd1s@users.noreply.github.com>
2023-08-13 17:40:03 +0900

    Challenge 10

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const btn = document.querySelector("button");
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+    
+const HIDDEN_CLASSNAME = "hidden";
+const USERID_KEY = "username";
+const USERPW_KEY = "password";
 
+loginButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+    localStorage.setItem(USERID_KEY, username);
+    localStorage.setItem(USERPW_KEY, password);
 
-function handleTitleClick(){
-    btn.classList.toggle("clicked");
-    if(btn.innerText === "확인"){
-        btn.innerText = "취소";
-    } else{
-        btn.innerText = "확인";
+    const savedUserName = localStorage.getItem(USERID_KEY);
+    const savedUserPw = localStorage.getItem(USERPW_KEY);
+
+    if (savedUserName === "wjddls" && savedUserPw === "12345") {
+        greeting.classList.remove(HIDDEN_CLASSNAME);
+        loginForm.classList.add(HIDDEN_CLASSNAME);
+        greeting.innerText = `Hello ${savedUserName}`;
+    } else {
+       alert("Please check your Id/PW!");
     }
-}
-
-
-btn.addEventListener("click", handleTitleClick);
+});
