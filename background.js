822f84d668fec480e1db3be590f7551bdee8c6e6
wjdd1s <141038368+wjdd1s@users.noreply.github.com>
2023-08-19 10:52:09 +0900

    background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `${chosenImage}`;
 bgImage.id = "background";
 bgImage.classList.add("hidden");
 
